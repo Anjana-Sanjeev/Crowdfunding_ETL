7061fd8c7f4d3c94b0ae756e249500ed90b79b30
Anjana Sanjeev <anjana.sanjeev@gmail.com>
2023-02-23 19:10:52 +1100

    SQL file updated

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,60 +1,48 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/HeIDIF
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+-- Creating 4 tables campaign, category, subcategory, contact
 
-
-CREATE TABLE "campaign" (
-    "cf_id" INT   NOT NULL,
-    "contact_id" INT   NOT NULL,
-    "company_name" VARCHAR(80)   NOT NULL,
-    "description" VARCHAR(100)   NOT NULL,
-    "goal" REAL   NOT NULL,
-    "pledged" REAL   NOT NULL,
-    "outcome" VARCHAR(20)   NOT NULL,
-    "backers_count" VARCHAR(20)   NOT NULL,
-    "country" VARCHAR(10)   NOT NULL,
-    "currency" VARCHAR(10)   NOT NULL,
-    "launched_date" DATE   NOT NULL,
-    "end_date" DATE   NOT NULL,
-    "category_id" VARCHAR(10)   NOT NULL,
-    "subcategory_id" VARCHAR(10)   NOT NULL,
-    CONSTRAINT "pk_campaign" PRIMARY KEY (
-        "cf_id"
-     )
+CREATE TABLE campaign (
+	cf_id INT PRIMARY KEY NOT NULL,
+	contact_id INT,
+	FOREIGN KEY (contact_id) REFERENCES contact (contact_id),
+	company_name VARCHAR(80) NOT NULL,
+	description VARCHAR(100),
+	goal REAL,
+	pledged REAL,
+	outcome VARCHAR(20),
+	backers_count VARCHAR(20),
+	country VARCHAR(10),
+	currency VARCHAR(10),
+	launched_date DATE,
+	end_date DATE,
+	category_id VARCHAR(10), 
+	FOREIGN KEY (category_id) REFERENCES category (category_id),
+	subcategory_id VARCHAR(10),
+	FOREIGN KEY (subcategory_id) REFERENCES subcategory (subcategory_id)
 );
 
-CREATE TABLE "category" (
-    "category_id" VARCHAR(10)   NOT NULL,
-    "category" VARCHAR(30)   NOT NULL,
-    CONSTRAINT "pk_category" PRIMARY KEY (
-        "category_id"
-     )
+CREATE TABLE category (
+	category_id VARCHAR(10) PRIMARY KEY NOT NULL,
+	category VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE "subcategory" (
-    "subcategory_id" VARCHAR(10)   NOT NULL,
-    "subcategory" VARCHAR(30)   NOT NULL,
-    CONSTRAINT "pk_subcategory" PRIMARY KEY (
-        "subcategory_id"
-     )
+CREATE TABLE subcategory (
+	subcategory_id VARCHAR(10) PRIMARY KEY NOT NULL,
+	subcategory VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE "contact" (
-    "contact_id" INT   NOT NULL,
-    "first_name" VARCHAR(50)   NOT NULL,
-    "last_name" VARCHAR(50)   NOT NULL,
-    "email" VARCHAR(80)   NOT NULL,
-    CONSTRAINT "pk_contact" PRIMARY KEY (
-        "contact_id"
-     )
+CREATE TABLE contact (
+	contact_id INT PRIMARY KEY NOT NULL,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	email VARCHAR(80) NOT NULL
 );
 
-ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
-REFERENCES "contact" ("contact_id");
+-- view the tables after importing data from csv files
 
-ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
-REFERENCES "category" ("category_id");
+SELECT * FROM campaign;
 
-ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
-REFERENCES "subcategory" ("subcategory_id");
+SELECT * FROM category;
 
+SELECT * FROM subcategory;
+
+SELECT * FROM contact;
